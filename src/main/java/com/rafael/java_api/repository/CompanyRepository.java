4027c78340bae60096fbf5416d6350ca5260dfe9
Rafael Crespim <rafa.cechinato@gmail.com>
2023-11-07 20:09:44 -0300

    Alterações

--- src/main/java/com/rafael/java_api/repository/CompanyRepository.java
+++ src/main/java/com/rafael/java_api/repository/CompanyRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 public interface CompanyRepository extends MongoRepository<CompanyModel, String> {
     // @Query("{'listUser.id': ?0 }")
     @Query("{'idUser.id': ?0 }")
-    List<CompanyModel> findByUserID(String name);
+    List<CompanyModel> findByUserID(String userId);
 
 }
